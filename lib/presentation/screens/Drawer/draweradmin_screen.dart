e8695c3da9a66fb8c2ec46a625399a922346e49b
Ninhnon <nguyentrongninh24072003@gmail.com>
2023-09-23 09:12:12 +0700

    devNinh fix

--- lib/presentation/screens/Drawer/draweradmin_screen.dart
+++ lib/presentation/screens/Drawer/draweradmin_screen.dart
@@ -3,11 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:traffic_solution_dsc/core/constraints/size_config.dart';
 import 'package:traffic_solution_dsc/models/cdm.dart';
 import 'package:traffic_solution_dsc/presentation/screens/HomeScreen/HomeScreen.dart';
-import 'package:traffic_solution_dsc/presentation/screens/searchScreen/searchSreen.dart';
 import 'package:traffic_solution_dsc/presentation/blocs/app/app_bloc.dart';
 import 'package:traffic_solution_dsc/models/user/user.dart';
 import 'package:traffic_solution_dsc/presentation/screens/homeAdmin/homeAdminScreen.dart';
-import 'package:traffic_solution_dsc/presentation/screens/searchScreen/cubit/search_cubit.dart';
+import 'package:traffic_solution_dsc/presentation/screens/Direction/chooseLocation.dart';
 
 class DrawerAdminPage extends StatefulWidget {
   const DrawerAdminPage({Key? key}) : super(key: key);
@@ -31,10 +30,7 @@ class _DrawerAdminPageState extends State<DrawerAdminPage> {
     } else if (currentPage == DrawerSections.map) {
       container = HomeScreen.provider();
     } else if (currentPage == DrawerSections.route) {
-      container = BlocProvider(
-        create: (context) => SearchCubit(),
-        child: SearchScreen(),
-      );
+      container = ChooseLocation();
     }
     SizeConfig().init(context);
 
